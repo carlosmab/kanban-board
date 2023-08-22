4cc15474c1abddf87530c711b1f2fa6b0d955d75
carlosmab <carlosm.araujob@gmail.com>
2023-08-22 18:02:42 -0500

    Create column basic logic

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useState } from 'react'
+import PlusIcon from '../icons/PlusIcon'
+import { Column } from '../types';
 
 function KanbanBoard() {
+  const [ columns, setColumns ] = useState<Column[]>([]);
+  console.log(columns);
   return (
     <div
       className="
@@ -16,6 +20,7 @@ function KanbanBoard() {
     >
       <div className="m-auto">
         <button
+          onClick={createNewColumn}
           className="
             h-[60px]
             w-[350px]
@@ -28,13 +33,30 @@ function KanbanBoard() {
             p-4
             ring-rose-500
             hover:ring-2
+            flex
+            gap-2
           "
         >
+          <PlusIcon />
           Add Column
         </button>
       </div>
     </div>
-  )
+  );
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    }
+
+    setColumns([...columns, columnToAdd]);
+  }
+}
+
+
+function generateId() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
 export default KanbanBoard
