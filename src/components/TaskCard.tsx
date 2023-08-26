8702611420184ce6fd5159e88c3efdb6a8ebddb0
carlosmab <carlosm.araujob@gmail.com>
2023-08-26 15:06:20 -0500

    Styled task card

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,4 @@
+import TrashIcon from '../icons/TrashIcon';
 import { Task } from '../types';
 
 interface Props {
@@ -15,15 +16,31 @@ function TaskCard(props: Props) {
         p-2.5
         h-[100px]
         min-h-[100px]
+        flex
         items-center
         text-left
         rounded-xl
         hover:ring-2
         hover:ring-rose-500
-        cursor-grabs
+        cursor-grab
+        relative
       "
     >
       {task.content}
+      <button
+        className="
+          stroke-white
+          absolute
+          right-4
+          top-1/2
+          -translate-y-1/2
+          bg-columnBackgroundColor
+          p-2
+          rounded
+        "
+      >
+        <TrashIcon />
+      </button>
     </div>
   )
 }
