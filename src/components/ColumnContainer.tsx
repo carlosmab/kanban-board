4788b77aea7694fd0ae98a3e1fa209b8aa17a7cd
carlosmab <carlosm.araujob@gmail.com>
2023-08-26 09:01:22 -0500

    Dragable element

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,13 +1,106 @@
-import { Column } from '../types';
+import TrashIcon from '../icons/TrashIcon';
+import { Column, Id } from '../types';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 interface Props {
   column: Column;
+  deleteColumn: (id: Id) => void;
 }
 
 function ColumnContainer(props: Props) {
-  const { column } = props;
+  const { column, deleteColumn } = props;
+
+  const { 
+    setNodeRef, 
+    attributes, 
+    listeners, 
+    transform,
+    transition } = useSortable((
+    {
+      id: column.id,
+      data: {
+        type: 'column',
+        column,
+      }
+    }
+  ))
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  }
+
   return (
-    <div>{column.title}</div>
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="
+        bg-columnBackgroundColor
+        w-[350px]
+        h-[500px]
+        max-h-[500px]
+        rounded-md
+        flex
+        flex-col
+      "
+    >
+      {/* {column.title} */}
+      <div
+        {...attributes}
+        {...listeners}
+        className="
+          bg-mainBackgroundColor
+          text-md
+          h-[60px]
+          cursor-grab
+          rounded-md
+          rounded-b-none
+          p-3
+          font-bold
+          border-columnBackgroundColor
+          border-4
+          flex
+          justify-between
+        "
+      >
+        <div className='flex gap-2'>
+          <div
+            className="
+              bg-mainBackgroundColor
+              flex
+              justify-center
+              items-center
+              px-2
+              py-1
+              text-sm
+              rounded-full
+            "
+          >
+            0
+          </div>
+          {column.title}
+        </div>
+        <button
+          onClick={() => {
+            deleteColumn(column.id);
+          }}
+          className="
+            stroke-gray-500
+            hover:stroke-white
+            hover:bg-columnBackgroundColor
+            rounded
+          "
+        >
+          <TrashIcon />
+        </button>
+      </div>
+
+
+      {/* column task container */}
+      <div className="flex flex-grow">Content</div>
+      <div>Footer</div>
+    </div>
   )
 }
 
--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
-import { Column } from "../types";
+import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
+import { DndContext } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
 
 function KanbanBoard() {
   const [columns, setColumns] = useState<Column[]>([]);
+  const columnsId = useMemo(() => columns.map((column) => column.id), [columns]);
 
   return (
     <div
@@ -19,34 +22,42 @@ function KanbanBoard() {
         px-[40px]
       "
     >
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map((column) => (
-            <ColumnContainer key={column.id} column={column} />
-          ))}
+      <DndContext>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map((column) => (
+                <ColumnContainer 
+                  key={column.id} 
+                  column={column} 
+                  deleteColumn={deleteColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={createNewColumn}
+            className="
+              h-[60px]
+              w-[350px]
+              min-w-[350px]
+              cursor-pointer
+              rounded-lg
+              bg-mainBackgroundColor
+              border-2
+              border-columnBackgroundColor
+              p-4
+              ring-rose-500
+              hover:ring-2
+              flex
+              gap-2
+            "
+          >
+            <PlusIcon />
+            Add Column
+          </button>
         </div>
-        <button
-          onClick={createNewColumn}
-          className="
-            h-[60px]
-            w-[350px]
-            min-w-[350px]
-            cursor-pointer
-            rounded-lg
-            bg-mainBackgroundColor
-            border-2
-            border-columnBackgroundColor
-            p-4
-            ring-rose-500
-            hover:ring-2
-            flex
-            gap-2
-          "
-        >
-          <PlusIcon />
-          Add Column
-        </button>
-      </div>
+      </DndContext>
     </div>
   );
 
@@ -57,6 +68,10 @@ function KanbanBoard() {
     };
 
     setColumns([...columns, columnToAdd]);
+  }
+
+  function deleteColumn(id: Id) {
+    setColumns(columns.filter((column) => column.id !== id));
   }
 }
 
